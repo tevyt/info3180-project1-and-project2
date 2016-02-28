d22a36b2371b990d55109de42e8b02fcdf202068
Travis Smith <travissmith94@hotmail.com>
2016-02-28 12:34:20 -0500

    set up views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from app import app
-
+from flask import url_for, redirect
 # from flask import render_template
 
 @app.route('/')
-def home():
-    return 'Flask App is ready!'
+def root():
+    return redirect(url_for('index'))
+
+@app.route('/profile' , methods=['GET' , 'POST'])
+def new():
+    return 'TO DO'
+
+@app.route('/profiles')
+def index():
+    return 'TO DO'
+
+@app.route('/profile/<user_id>')
+def show(user_id):
+    return user_id
 
