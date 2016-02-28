1628ca76a33032403721d38fb69384a7d349398a
Travis Smith <travissmith94@hotmail.com>
2016-02-28 12:21:46 -0500

    Connect to postgres database

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/project1'
 db = SQLAlchemy(app)
 from app import views
 from app.models import User
