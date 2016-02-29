b3b2f43fcab57408d77d7b43d5d219b742eda4b1
Travis Smith <travissmith94@hotmail.com>
2016-02-28 21:34:29 -0500

    Testing problems

--- app/views.py
+++ app/views.py
@@ -13,30 +13,32 @@ def root():
 
 @app.route('/profile' , methods=['GET' , 'POST'])
 def new():
-    form = UserForm()
+    return 'Test'
+# def new():
+#     form = UserForm()
     
-    if request.headers['Content-Type'] == 'application/json':
-        users = db.session.query(User).all()
-        return user_list_json(users)
-    if request.method == 'GET':
-        return render_template('new.html', form=form)
-    elif form.validate():
-        firstname = form.firstname.data
-        lastname = form.lastname.data
-        username = form.username.data
-        sex = form.sex.data
-        age = form.age.data
-        image = form.image.data
-        user = User(username ,firstname , lastname ,  age , sex ,'')
-        filename = str(getrandbits(20)) + image.filename
-        image.save(os.path.join("app/static", filename))
-        user.file_location = filename
-        db.session.add(user)
-        db.session.commit()
-        flash('New Profile created')
-        return redirect(url_for('show' , user_id=user.user_id))
-    else:
-        return render_template('new.html' , form=form,errors=form.errors.items())
+#     if request.headers['Content-Type'] == 'application/json':
+#         users = db.session.query(User).all()
+#         return user_list_json(users)
+#     if request.method == 'GET':
+#         return render_template('new.html', form=form)
+#     elif form.validate():
+#         firstname = form.firstname.data
+#         lastname = form.lastname.data
+#         username = form.username.data
+#         sex = form.sex.data
+#         age = form.age.data
+#         image = form.image.data
+#         user = User(username ,firstname , lastname ,  age , sex ,'')
+#         filename = str(getrandbits(20)) + image.filename
+#         image.save(os.path.join("app/static", filename))
+#         user.file_location = filename
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('New Profile created')
+#         return redirect(url_for('show' , user_id=user.user_id))
+#     else:
+#         return render_template('new.html' , form=form,errors=form.errors.items())
 
 @app.route('/profiles')
 def index():
