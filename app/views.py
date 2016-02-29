52abec8ff80cbba3ddfb85c3936b420740c4a45b
Travis Smith <travissmith94@hotmail.com>
2016-02-28 20:28:19 -0500

    Edge case protection for json requests

--- app/views.py
+++ app/views.py
@@ -14,6 +14,10 @@ def root():
 @app.route('/profile' , methods=['GET' , 'POST'])
 def new():
     form = UserForm()
+    
+    if request.headers['Content-Type'] == 'application/json':
+        users = db.session.query(User).all()
+        return user_list_json(users)
     if request.method == 'GET':
         return render_template('new.html', form=form)
     elif form.validate():
@@ -38,30 +42,38 @@ def new():
 def index():
     users = db.session.query(User).all()
     if request.headers['Content-Type'] == 'application/json':
-        user_json = []
-        for user in users:
-            user_json.append({'username': user.username , 'userid': user.user_id})
-        return Response(json.dumps(user_json) , mimetype='application/json')
+        return user_list_json(users)
         
     if len(users) == 0:
         return 'No Registered Users'
     return render_template('index.html' , users=users)
 
 
-@app.route('/profile/<user_id>')
+@app.route('/profile/<user_id>' , methods=['GET' , 'POST'])
 def show(user_id):
     user = db.session.query(User).filter_by(user_id=user_id).first()
     if user == None:
+        if request.headers['Content-Type'] == 'application/json':
+            return jsonify(message="Resource not found") , 404
         return render_template('404.html'), 404
     date = format_date(user.added_on)
-    if request.headers['Content-Type'] == 'application/json':
+    if request.headers['Content-Type'] == 'application/json' or request.method == 'POST':
         return jsonify(userid=user.user_id , username=user.username , image=user.file_location , age=user.age , profile_added_on=date)
     return render_template('show.html', user=user , date=date)
 
 @app.errorhandler(404)
 def page_not_found(e):
+    if request.headers['Content-Type'] == 'application/json':
+        return jsonify(message="Resource not found") , 404
     return render_template('404.html'), 404
+
 
 def format_date(date):
     return date.strftime('%a %W %b %Y')
+
+def user_list_json(users):
+    user_json = []
+    for user in users:
+        user_json.append({'username': user.username , 'userid': user.user_id})
+    return Response(json.dumps(user_json) , mimetype='application/json')
 
