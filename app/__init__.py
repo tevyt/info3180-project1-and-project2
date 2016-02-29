18ba0987f631503d36cbd6f5ce05b85bc779a649
Travis Smith <travissmith94@hotmail.com>
2016-02-28 21:24:18 -0500

    Changed to heroku's database

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mnIcdwVxqc80HQ0nYOtS'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/project1'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 from app import views
 from app.models import User
