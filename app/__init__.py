760745451a7bc6ceca3f9d25443fb69f22817b05
Travis Smith <travissmith94@hotmail.com>
2016-02-28 21:42:53 -0500

    Added logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import sys
+import logging
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 app.config['SECRET_KEY'] = 'mnIcdwVxqc80HQ0nYOtS'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
