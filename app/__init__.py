7532f81a74b317160b8730ed4c154cb85f85e8c0
Travis Smith <travissmith94@hotmail.com>
2016-02-28 21:53:39 -0500

    Got Heroku db url

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 app.config['SECRET_KEY'] = 'mnIcdwVxqc80HQ0nYOtS'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://buaxjsebsacswt:_i8t_TBnHU5qRwxsDEU0ilUKjg@ec2-54-83-12-22.compute-1.amazonaws.com:5432/ddhf81amgep2s5"
 db = SQLAlchemy(app)
 from app import views
 from app.models import User
